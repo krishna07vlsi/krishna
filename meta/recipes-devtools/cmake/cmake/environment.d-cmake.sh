4c2723d09e6d865f93610f3c4989df81c3e99b16
rnarayanan <rajkumar.narayanan@lnttechservices.com>
2017-11-06 06:26:10 +0000

    RDKBEMU-713 : Upgrade cmake version for WPE failure in morty builds
    Reason for change: Upgrade cmake to 3.8.2 from 3.6.2 for morty
    to avoid wpe failure in arris xi6
    Test Procedure: Build and Verify
    Risks: None
    
    (Cherry-picked from upstream commit acf1fd0fa9888862ede33f503e879264b6311510)
    
    Change-Id: I063fa25980e084ce4389e24b1a167450d73ae6d9
    Signed-off-by: rnarayanan <rajkumar.narayanan@lnttechservices.com>
    Signed-off-by: rajakumaran.a <rajakumaran.a@lnttechservices.com>

--- meta/recipes-devtools/cmake/cmake/environment.d-cmake.sh
+++ meta/recipes-devtools/cmake/cmake/environment.d-cmake.sh
@@ -1,1 +1,6 @@
 alias cmake="cmake -DCMAKE_TOOLCHAIN_FILE=$OECORE_NATIVE_SYSROOT/usr/share/cmake/OEToolchainConfig.cmake"
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> c868097eb3... RDKBEMU-557 : cmake (3.8.2) Moved to openembedded-core
