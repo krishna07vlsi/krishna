b5cb23cb71302f739d8a395cb1da1c79ea09de14
jkkuriakose <Josekutty_Kuriakose@cable.comcast.com>
2018-06-20 07:44:04 +0000

    RDK-22157 Remove patch message
    Reason for change: Earlier check-ins added a patch by mistake to this file
    Test Procedure: populate_sdk task should execute without any issues
    Risks: None
    
    (Cherry-picked from upstream commit ea9a4cf06e0c23c1a90faa8938bd5e4b955cf692)
    
    Change-Id: I60baee6308ebee828dd990d382814b109cb25076
    Signed-off-by: jkkuriakose <Josekutty_Kuriakose@cable.comcast.com>

--- meta/recipes-devtools/cmake/cmake/environment.d-cmake.sh
+++ meta/recipes-devtools/cmake/cmake/environment.d-cmake.sh
@@ -1,6 +1,1 @@
 alias cmake="cmake -DCMAKE_TOOLCHAIN_FILE=$OECORE_NATIVE_SYSROOT/usr/share/cmake/OEToolchainConfig.cmake"
-<<<<<<< HEAD
-=======
-
-
->>>>>>> c868097eb3... RDKBEMU-557 : cmake (3.8.2) Moved to openembedded-core
